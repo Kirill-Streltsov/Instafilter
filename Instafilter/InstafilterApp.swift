e4766d94a871b754786588790ed8b13d71a2a335
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-08-02 00:36:48 +0200

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Kirill Streltsov on 02.08.23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
